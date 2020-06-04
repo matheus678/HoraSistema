7286ffdd2d18dea624f9577e2d6d5f2baeb928ae
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-06-04 18:37:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            [console: HoraSistema] Exiba a data e a hora atual do sistema.
+            Ex.:
+            Neste momento são exatamente 13/05/2020 14:58:13.
+            */
+            
+            DateTime HorarioAtual = DateTime.Now;
+        
+            Console.WriteLine($"Horário atual correspondente a sua localização: {HorarioAtual}");
+        }
+    }
+}
